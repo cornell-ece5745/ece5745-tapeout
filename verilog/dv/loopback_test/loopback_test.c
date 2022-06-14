7b6b70e37514f922883d86884f5643cf6f975001
acm289 <acm289@cornell.edu>
2022-06-14 18:15:01 -0400

    Reverted back to firmware testing

--- verilog/dv/loopback_test/loopback_test.c
+++ verilog/dv/loopback_test/loopback_test.c
@@ -31,101 +31,101 @@ int i;
 
 void main()
 {
-    //     /* Set up the housekeeping SPI to be connected internally so	*/
-	// /* that external pin changes don't affect it.			*/
+        /* Set up the housekeeping SPI to be connected internally so	*/
+	/* that external pin changes don't affect it.			*/
 
-	// // reg_spimaster_config = 0xa002;	// Enable, prescaler = 2,
-    //     reg_spi_enable = 1;
-    //                                     // connect to housekeeping SPI
+	// reg_spimaster_config = 0xa002;	// Enable, prescaler = 2,
+        reg_spi_enable = 1;
+                                        // connect to housekeeping SPI
 
-	// // Connect the housekeeping SPI to the SPI master
-	// // so that the CSB line is not left floating.  This allows
-	// // all of the GPIO pins to be used for user functions.
+	// Connect the housekeeping SPI to the SPI master
+	// so that the CSB line is not left floating.  This allows
+	// all of the GPIO pins to be used for user functions.
 
 
-	// // All GPIO pins are configured to be output
-	// // Used to flad the start/end of a test 
+	// All GPIO pins are configured to be output
+	// Used to flad the start/end of a test 
 
-    //     reg_mprj_io_31 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     reg_mprj_io_30 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     reg_mprj_io_29 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     reg_mprj_io_28 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     // // reg_mprj_io_27 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     // // reg_mprj_io_26 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     // // reg_mprj_io_25 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     // // reg_mprj_io_24 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     // // reg_mprj_io_23 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     // // reg_mprj_io_22 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     // // reg_mprj_io_21 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     // // reg_mprj_io_20 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     // // reg_mprj_io_19 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     // // reg_mprj_io_18 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     // // reg_mprj_io_17 = GPIO_MODE_MGMT_STD_OUTPUT;
-    //     // // reg_mprj_io_16 = GPIO_MODE_MGMT_STD_OUTPUT;
+        reg_mprj_io_31 = GPIO_MODE_MGMT_STD_OUTPUT;
+        reg_mprj_io_30 = GPIO_MODE_MGMT_STD_OUTPUT;
+        reg_mprj_io_29 = GPIO_MODE_MGMT_STD_OUTPUT;
+        reg_mprj_io_28 = GPIO_MODE_MGMT_STD_OUTPUT;
+        // // reg_mprj_io_27 = GPIO_MODE_MGMT_STD_OUTPUT;
+        // // reg_mprj_io_26 = GPIO_MODE_MGMT_STD_OUTPUT;
+        // // reg_mprj_io_25 = GPIO_MODE_MGMT_STD_OUTPUT;
+        // // reg_mprj_io_24 = GPIO_MODE_MGMT_STD_OUTPUT;
+        // // reg_mprj_io_23 = GPIO_MODE_MGMT_STD_OUTPUT;
+        // // reg_mprj_io_22 = GPIO_MODE_MGMT_STD_OUTPUT;
+        // // reg_mprj_io_21 = GPIO_MODE_MGMT_STD_OUTPUT;
+        // // reg_mprj_io_20 = GPIO_MODE_MGMT_STD_OUTPUT;
+        // // reg_mprj_io_19 = GPIO_MODE_MGMT_STD_OUTPUT;
+        // // reg_mprj_io_18 = GPIO_MODE_MGMT_STD_OUTPUT;
+        // // reg_mprj_io_17 = GPIO_MODE_MGMT_STD_OUTPUT;
+        // // reg_mprj_io_16 = GPIO_MODE_MGMT_STD_OUTPUT;
 
-    //     // // reg_mprj_io_15 = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_14 = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_13 = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_12 = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_11 = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_10 = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_9  = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_8  = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_7  = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_5  = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_4  = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_3  = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_2  = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_1  = GPIO_MODE_USER_STD_OUTPUT;
-    //     // // reg_mprj_io_0  = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_15 = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_14 = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_13 = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_12 = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_11 = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_10 = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_9  = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_8  = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_7  = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_5  = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_4  = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_3  = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_2  = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_1  = GPIO_MODE_USER_STD_OUTPUT;
+        // // reg_mprj_io_0  = GPIO_MODE_USER_STD_OUTPUT;
 
-    //     // reg_mprj_io_14 = GPIO_MODE_USER_STD_OUTPUT; //Adapter Parity
-    //     // reg_mprj_io_26 = GPIO_MODE_USER_STD_INPUT_NOPULL; //Clock
-    //     // reg_mprj_io_12 = GPIO_MODE_USER_STD_INPUT_NOPULL; //Loopthrough Select
-    //     // reg_mprj_io_13 = GPIO_MODE_USER_STD_OUTPUT; //Minion Parity
-    //     // reg_mprj_io_27 = GPIO_MODE_USER_STD_INPUT_NOPULL; //Reset
-    //     // reg_mprj_io_15 = GPIO_MODE_USER_STD_INPUT_NOPULL; //SPI_CS
-    //     // reg_mprj_io_17 = GPIO_MODE_USER_STD_OUTPUT; //SPI_MISO
-    //     // reg_mprj_io_18 = GPIO_MODE_USER_STD_INPUT_NOPULL; //SPI_MOSI
-    //     // reg_mprj_io_16 = GPIO_MODE_USER_STD_INPUT_NOPULL; //SPI_SCLK
+        reg_mprj_io_14 = GPIO_MODE_USER_STD_OUTPUT; //Adapter Parity
+        reg_mprj_io_26 = GPIO_MODE_USER_STD_INPUT_NOPULL; //Clock
+        reg_mprj_io_12 = GPIO_MODE_USER_STD_INPUT_NOPULL; //Loopthrough Select
+        reg_mprj_io_13 = GPIO_MODE_USER_STD_OUTPUT; //Minion Parity
+        reg_mprj_io_27 = GPIO_MODE_USER_STD_INPUT_NOPULL; //Reset
+        reg_mprj_io_15 = GPIO_MODE_USER_STD_INPUT_NOPULL; //SPI_CS
+        reg_mprj_io_17 = GPIO_MODE_USER_STD_OUTPUT; //SPI_MISO
+        reg_mprj_io_18 = GPIO_MODE_USER_STD_INPUT_NOPULL; //SPI_MOSI
+        reg_mprj_io_16 = GPIO_MODE_USER_STD_INPUT_NOPULL; //SPI_SCLK
         
 
-    //     /* Apply configuration */
-    //     reg_mprj_xfer = 1;
-    //     while (reg_mprj_xfer == 1);
+        /* Apply configuration */
+        reg_mprj_xfer = 1;
+        while (reg_mprj_xfer == 1);
 
-	// // Configure All LA probes as inputs to the cpu 
-	// reg_la0_oenb = reg_la0_iena = 0x00000000;    // [31:0]
-	// reg_la1_oenb = reg_la1_iena = 0x00000000;    // [63:32]
-	// reg_la2_oenb = reg_la2_iena = 0x00000000;    // [95:64]
-	// reg_la3_oenb = reg_la3_iena = 0x00000000;    // [127:96]
+	// Configure All LA probes as inputs to the cpu 
+	reg_la0_oenb = reg_la0_iena = 0x00000000;    // [31:0]
+	reg_la1_oenb = reg_la1_iena = 0x00000000;    // [63:32]
+	reg_la2_oenb = reg_la2_iena = 0x00000000;    // [95:64]
+	reg_la3_oenb = reg_la3_iena = 0x00000000;    // [127:96]
 
-	// // Flag start of the test
-	// reg_mprj_datal = 0xA0000000;
+	// Flag start of the test
+	reg_mprj_datal = 0xA0000000;
 
-	// // Configure LA[64] LA[65] as outputs from the cpu
-	// reg_la2_oenb = reg_la2_iena = 0x00000003; 
+	// Configure LA[64] LA[65] as outputs from the cpu
+	reg_la2_oenb = reg_la2_iena = 0x00000003; 
 
-	// // Set clk & reset to one
-	// reg_la2_data = 0x00000003;
+	// Set clk & reset to one
+	reg_la2_data = 0x00000003;
 
-    //     // DELAY
-    //     for (i=0; i<5; i=i+1) {}
+        // DELAY
+        for (i=0; i<5; i=i+1) {}
 
-	// // Toggle clk & de-assert reset
-	// for (i=0; i<11; i=i+1) {
-	// 	clk = !clk;
-	// 	reg_la2_data = 0x00000000 | clk;
-	// }
+	// Toggle clk & de-assert reset
+	for (i=0; i<11; i=i+1) {
+		clk = !clk;
+		reg_la2_data = 0x00000000 | clk;
+	}
 
-    //     // reg_mprj_datal = 0xAB610000;
+        // reg_mprj_datal = 0xAB610000;
 
-    //     // while (1){
-    //     //         if (reg_la0_data_in >= 0x05) {
-    //     //                 reg_mprj_datal = 0xAB610000;
-    //     //                 break;
-    //     //         }
+        // while (1){
+        //         if (reg_la0_data_in >= 0x05) {
+        //                 reg_mprj_datal = 0xAB610000;
+        //                 break;
+        //         }
                 
-    //     // }
+        // }
 
 }
